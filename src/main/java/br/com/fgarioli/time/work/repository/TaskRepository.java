a1b775cb97aa06d5f09e4f954f47d762b331c1b7
Fernando Garioli <f.ribeirogarioli@gmail.com>
2018-12-22 01:08:25 -0200

    Adição Anotação Repository

--- src/main/java/br/com/fgarioli/time/work/repository/TaskRepository.java
+++ src/main/java/br/com/fgarioli/time/work/repository/TaskRepository.java
@@ -7,11 +7,13 @@ package br.com.fgarioli.time.work.repository;
 
 import br.com.fgarioli.time.work.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author fernando
  */
+@Repository
 public interface TaskRepository  extends JpaRepository<Task, Integer> {
     
 }
